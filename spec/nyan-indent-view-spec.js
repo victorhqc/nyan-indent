8ef81f2e0a33e0b1d39e0ff6970be9289d9d521b
Victor Quiroz <victorhqc@gmail.com>
2017-10-13 17:21:47 +0200

    Initial ugly approach

---
+++ spec/nyan-indent-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import NyanIndentView from '../lib/nyan-indent-view';
+
+describe('NyanIndentView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
