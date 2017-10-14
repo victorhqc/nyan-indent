08f75f81aa887c553ba4898ccbec405ee83c0b43
Victor Quiroz <victorhqc@gmail.com>
2017-10-14 19:36:51 +0200

    Clean a bit the default test

--- spec/helpers.js
+++ spec/helpers.js
@@ -1,17 +1,21 @@
 'use babel';
 
-/* eslint import/prefer-default-export: 0 */
-
-export const prepareEditor = atom => new Promise((resolve) => {
-  atom.workspace.open(`${__dirname}/text-helper.txt`).then((editor) => {
-    editor.setText(`
-no tab here
-  tabulation of 1
-    tabulation of 2
-      tabulation of 3
-        tabulation of 4
-          tabulation of 5
-`);
-    editor.save().then(resolve(editor));
-  });
+export const setText = (editor, text) => new Promise((resolve) => {
+  editor.setText(text);
+  editor.save().then(resolve(editor));
 });
+
+export const openTestFile = atom => atom.workspace.open(`${__dirname}/text-helper.txt`);
+
+export const prepareEditor = (atom, text) =>
+  openTestFile(atom)
+    .then(editor => setText(editor, text));
+
+export const findDecorations = editor => editor.getDecorations({
+  type: 'text',
+  class: 'nyan-indent',
+});
+
+export const getColors = atom => atom.config.get('nyan-indent.colors');
+
+export const activatePackage = atom => atom.packages.activatePackage('nyan-indent');
--- spec/nyan-indent-spec.js
+++ spec/nyan-indent-spec.js
@@ -1,55 +1,69 @@
 'use babel';
 
 import filter from 'lodash/filter';
-import { prepareEditor } from './helpers';
-import NyanIndent from '../lib/nyan-indent';
+import {
+  prepareEditor,
+  findDecorations,
+  getColors,
+  activatePackage,
+} from './helpers';
+
+const text = `
+no tab here
+  tabulation of 1
+    tabulation of 2
+      tabulation of 3
+        tabulation of 4
+          tabulation of 5
+`;
 
 // Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
 //
 // To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
 // or `fdescribe`). Remove the `f` to unfocus the block.
 
-
 const findDecorationByColor = (decorations, color) =>
   filter(decorations, decoration => decoration.properties.style.backgroundColor === color);
 
 describe('NyanIndent', () => {
-  it('Should paint on render', () =>
-    prepareEditor(atom).then((editor) => {
-      atom.packages.activatePackage('nyan-indent').then(() => {
-        const decorations = editor.getDecorations({
-          type: 'text',
-          class: 'nyan-indent',
-        });
+  it('Should paint on render', () => {
+    prepareEditor(atom, text).then((editor) => {
+      activatePackage(atom).then(() => {
+        const decorations = findDecorations(editor);
         expect(decorations.length).toBe(15);
 
-        const colors = atom.config.get('nyan-indent.colors');
+        const colors = getColors(atom);
 
+        // There should be 5 red "indentations" from top to bottom
         const firstTabDecorations = findDecorationByColor(decorations, colors[0]);
         expect(
           firstTabDecorations.length,
         ).toBe(5);
 
+        // There should be 4 yellow "indentations" from top to bottom
         const secondTabDecorations = findDecorationByColor(decorations, colors[1]);
         expect(
           secondTabDecorations.length,
         ).toBe(4);
 
+        // There should be 3 green "indentations" from top to bottom
         const thirdTabDecorations = findDecorationByColor(decorations, colors[2]);
         expect(
           thirdTabDecorations.length,
         ).toBe(3);
 
+        // There should be 2 blue "indentations" from top to bottom
         const fourthTabDecorations = findDecorationByColor(decorations, colors[3]);
         expect(
           fourthTabDecorations.length,
         ).toBe(2);
 
+        // There should be 1 purple "indentation" from top to bottom
         const fifthTabDecorations = findDecorationByColor(decorations, colors[4]);
         expect(
           fifthTabDecorations.length,
         ).toBe(1);
       });
-    }),
-  );
+    });
+  });
 });
